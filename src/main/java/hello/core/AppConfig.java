b90fbd35d96df3b35e3d11821d86bacf07dac5ff
Youmee <137977582+YoumeePark@users.noreply.github.com>
2025-07-15 20:38:07 +0900

    Update AppConfig.java

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -11,29 +11,25 @@ import hello.core.order.OrderServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-//@Configuration
+@Configuration
 public class AppConfig {
 
     @Bean
     public MemberService memberService() {
-        //1번
         System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
-    public OrderService orderService() {
-        //1번
-        System.out.println("call AppConfig.orderService");
-        //return new OrderServiceImpl(memberRepository(), discountPolicy());
-        return null;
+    public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
+        return new MemoryMemberRepository();
     }
 
     @Bean
-    public MemberRepository memberRepository() {
-        //2번? 3번?
-        System.out.println("call AppConfig.memberRepository");
-        return new MemoryMemberRepository();
+    public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
     @Bean
